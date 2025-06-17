b8368285eb2220953bfa45de3b2aa31e2e470d70
Naresh0348 <nareshbaskar11@gmail.com>
2025-06-17 12:11:43 +0530

    final

--- Sp_agg/stockprice/src/App.jsx
+++ Sp_agg/stockprice/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import stockprice from './stockprice';
 import hmap from './hmap';
 
@@ -9,6 +9,11 @@ function App() {
       <div>
         <h1>Stock aggregation</h1>
       </div>
+      <nav className="mb-4">
+          <Link to="/" >Stock Page</Link>
+          <br/><br/>
+          <Link to="/heatmap">Heatmap</Link>
+        </nav>
       <Routes>
         <Route path='/' element={<stockprice/>}/>
         <Route path='/heatmap' element={<hmap/>}/>
